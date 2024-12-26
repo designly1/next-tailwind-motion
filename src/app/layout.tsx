5f30770e72e503f10de77eec97fb344f17912a95
Jay Simons <jay@designly.biz>
2024-12-26 02:33:29 -0600

    init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,11 +23,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="h-full">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-800 text-zinc-300 h-full`}
       >
-        {children}
+        <main className="h-full flex flex-col">{children}</main>
       </body>
     </html>
   );
